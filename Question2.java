44c2ca6348da5d87aa3a099d36f19fc4f60052b7
Zenusha <nirkajishakya23@gmail.com>
2025-04-27 12:19:32 +0545

    final

--- Question2.java
+++ Question2.java
@@ -6,8 +6,8 @@ public class Question2 {
         int number = scanner.nextInt();
         int sum = 0;
         while (number > 0) {
-            sum += number % 10; // Add the last digit to the sum
-            number /= 10; // Remove the last digit from the number
+            sum += number % 10;
+            number /= 10;
         }
         System.out.println("The sum of the digits is: " + sum);
     }
