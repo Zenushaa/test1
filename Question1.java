2758a9e1f327e195086fc1b5c208b007ea07f247
Zenusha <nirkajishakya23@gmail.com>
2025-04-27 12:34:54 +0545

    reverse

--- Question1.java
+++ Question1.java
@@ -4,9 +4,13 @@ import java.util.Scanner;
 public class Question1 {
     public static void main(String[] args) {
         // Write a Java program to reverse a word provided by the user.
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a word here: ");
         String word = scanner.nextLine();
+        String reversedWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+        System.out.println("The reversed word is: " + reversedWord);
     }
 }
